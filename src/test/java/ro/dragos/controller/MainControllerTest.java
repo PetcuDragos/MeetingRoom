f990426e1845e968c44d0967b8a2914acce71769
Petcu Dragos <dyenigma@gmail.com>
2023-07-09 18:21:30 +0300

    [R-0.0.4] Added unit tests for MainController

---
+++ src/test/java/ro/dragos/controller/MainControllerTest.java
@@ -0,0 +1,88 @@
+package ro.dragos.controller;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import ro.dragos.dto.RoomDto;
+import ro.dragos.model.Room;
+import ro.dragos.repository.RoomRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MainControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private RoomRepository repository;
+
+    @Test
+    public void getHelloMessageTest() throws Exception {
+        mockMvc.perform(get("/start"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("hello!")));
+    }
+
+    @Test
+    public void getRoomsTest() throws Exception {
+
+        when(repository.getRooms()).thenReturn(List.of(new Room(1L, "Room1", new ArrayList<>())));
+
+        mockMvc.perform(get("/room"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name", is("Room1")));
+    }
+
+    @Test
+    public void addRoomTest() throws Exception {
+
+        RoomDto roomDto = new RoomDto(1L,"Room1", new ArrayList<>());
+
+        when(repository.addRoom(Mockito.any(Room.class))).thenReturn(true);
+
+        mockMvc.perform(post("/room")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toJson(roomDto)))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("true")));
+    }
+
+    @Test
+    public void updateRoomTest() throws Exception {
+
+        RoomDto roomDto = new RoomDto(1L,"Room1", new ArrayList<>());
+
+        when(repository.updateRoom(Mockito.any(Long.class), Mockito.any(Room.class))).thenReturn(true);
+
+        mockMvc.perform(put("/room/{id}", 1)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(toJson(roomDto)))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("true")));
+    }
+
+    public String toJson(Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
